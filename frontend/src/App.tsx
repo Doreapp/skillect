e006642327d8d719a303c21ba1c4a3e98ff227b5
AntoineMdn <antoine.mandin.am@gmail.com>
2022-10-22 19:03:15 +0200

    :hammer: Fix imported script

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import Slider from '@mui/material/Slider';
 import PopoverMenu from './PopoverMenu';
 import ProTip from './ProTip';
 
@@ -22,20 +21,12 @@ function Copyright() {
 export default function App() {
   return (
     <Container maxWidth="sm">
-      <div className="my-4">
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App + Tailwind CSS example
         </Typography>
-        <Slider
-          className="my-4"
-          defaultValue={30}
-          classes={{ active: 'shadow-none' }}
-          slotProps={{ thumb: { className: 'hover:shadow-none' } }}
-        />
         <PopoverMenu />
         <ProTip />
         <Copyright />
-      </div>
     </Container>
   );
 }
