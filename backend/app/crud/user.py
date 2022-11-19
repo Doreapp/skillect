99517d3d6ff2defc8444895a2ea31b4baf2ff4b7
AntoineMdn <antoine.mandin.am@gmail.com>
2022-11-19 12:29:04 +0100

    :hammer: Introduce user-based API dependencies

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -82,13 +82,15 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             return None
         return user
 
-    def is_active(self, user: User) -> bool:
+    @staticmethod
+    def is_active(user: User) -> bool:
         """
         Return if given `user` is active
         """
         return user.is_active
 
-    def is_superuser(self, user: User) -> bool:
+    @staticmethod
+    def is_superuser(user: User) -> bool:
         """
         Return if given `user` is super user
         """
