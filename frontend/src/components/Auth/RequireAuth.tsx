4389fd4c2d98890cc86982276ab65945643e32df
AntoineMdn <antoine.mandin.am@gmail.com>
2022-11-20 19:39:42 +0100

    :bug: Fix redirection to login page

--- frontend/src/components/Auth/RequireAuth.tsx
+++ frontend/src/components/Auth/RequireAuth.tsx
@@ -21,7 +21,7 @@ export default function RequireAuth(props: Props): JSX.Element {
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    return <Navigate to="/login" state={{from: location}} replace />
+    return <Navigate to="/admin/login" state={{from: location}} replace />
   }
 
   return props.children
