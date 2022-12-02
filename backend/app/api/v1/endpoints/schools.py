aef42bdc1eaf02d20911f35247928522fe61e9ca
AntoineMdn <antoine.mandin.am@gmail.com>
2022-12-02 18:13:04 +0100

    :sparkles: Introduce school creation

--- backend/app/api/v1/endpoints/schools.py
+++ backend/app/api/v1/endpoints/schools.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import Session
 
 from app.api import deps
 from app.crud.school import CRUDSchool
-from app.schemas.school import School
+from app.models.user import User
+from app.schemas.school import School, SchoolCreate
 
 
 def get_router() -> APIRouter:
@@ -25,5 +26,20 @@ def get_router() -> APIRouter:
         Retrieve schools.
         """
         return school_crud.get_multi(db, skip=skip, limit=limit)
+
+    @router.post("/", response_model=School)
+    def create_school(
+        *,
+        db: Session = Depends(deps.get_db),
+        school_in: SchoolCreate,
+        _: User = Depends(deps.get_current_superuser),
+    ) -> School:
+        """
+        Create new school.
+
+        Needs to be superuser.
+        """
+        school = school_crud.create(db, obj_in=school_in)
+        return school
 
     return router
