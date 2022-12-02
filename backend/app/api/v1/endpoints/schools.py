22bfd9bff2de2f612a374e1f325f4dc77cd74533
AntoineMdn <antoine.mandin.am@gmail.com>
2022-12-02 18:24:03 +0100

    :sparkles: Introduce school get by id and update

--- backend/app/api/v1/endpoints/schools.py
+++ backend/app/api/v1/endpoints/schools.py
@@ -1,14 +1,14 @@
 """Schools API entrypoints"""
 
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.api import deps
 from app.crud.school import CRUDSchool
 from app.models.user import User
-from app.schemas.school import School, SchoolCreate
+from app.schemas.school import School, SchoolCreate, SchoolUpdate
 
 
 def get_router() -> APIRouter:
@@ -40,6 +40,44 @@ def get_router() -> APIRouter:
         Needs to be superuser.
         """
         school = school_crud.create(db, obj_in=school_in)
+        return school
+
+    @router.get("/{school_id}", response_model=School)
+    def read_school_by_id(
+        school_id: int,
+        db: Session = Depends(deps.get_db),
+    ) -> Optional[School]:
+        """
+        Get a single school from its id.
+        """
+        school = school_crud.get(db, id=school_id)
+        if not school:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No school with this id exists",
+            )
+        return school
+
+    @router.put("/{school_id}", response_model=School)
+    def update_school(
+        *,
+        db: Session = Depends(deps.get_db),
+        school_id: int,
+        school_in: SchoolUpdate,
+        _: User = Depends(deps.get_current_superuser),
+    ) -> Optional[School]:
+        """
+        Update a school.
+
+        Needs to be superuser.
+        """
+        school = school_crud.get(db, id=school_id)
+        if not school:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No school with this id exists",
+            )
+        school = school_crud.update(db, db_obj=school, obj_in=school_in)
         return school
 
     return router
